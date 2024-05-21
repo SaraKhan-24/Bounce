ff06e4b300bcd595c40660a4541453be707d0f3b
Sara Khan <sarakhan31354@gmail.com>
2024-05-21 20:04:57 +0500

    removing character movement

--- Bounce/index.js
+++ Bounce/index.js
@@ -36,36 +36,8 @@ var tileTypes = {
     var ringlowery=mapH+1;
     var ringlowerx=mapW+1;
    
-    var lastFrameTime=0;
-    var keysDown={
-        37:false,//left
-        38:false,//down
-        39:false,
-        40:false
-    };
-    var player=new Character();
+    
 
-    function Character(){
-        this.tileFrom=[1,1];
-        this.tileTo=[1,1];
-        this.timeMoved=0;
-        this.dimensions=[30,30];
-        this.position=[45,45];
-        this.delayMove=700;
-
-    }
-    Character.prototype.placeAt=function(x,y){
-        this.tileFrom=[x,y];
-        this.tileTo=[x,y];
-        this.position=[((tileW*x)+
-            ((tileW-this.dimensions[0])/2)),
-            ((tileH*y)+
-            (tileH-this.dimensions[1])/2)];
-    };
-    // Character.prototype.processMovement=function(t){
-    //     if(this.tileFrom[0]==this.tileTo[0]&&
-    //     this.tileFrom[1]==)
-    // };
 tileTypes[0].src = 'tile.jpg';  // Source for tile type 0
 tileTypes[1].src = 'popper.png';
 tileTypes[2].src = 'deflator.jpeg';
@@ -199,16 +171,8 @@ function drawHorizontalSmallRing(x, y) {
 
   function drawGame(){
     if(ctx===null){ return;}
-    var sec=Math.floor(Date.now()/1000);
-
-    if(sec!=currentSecond){
-        currentSecond= sec;
-        framesLastSecond=frameCount;
-        frameCount=1;
-
-    }else{
-        frameCount++;
-    }
+    
+    
     for(var y=0; y<mapH;y++){
         for(var x=0; x<mapW ; x++){
             switch(gameMap[((y*mapW)+x)]){
@@ -273,7 +237,8 @@ function drawHorizontalSmallRing(x, y) {
 
         }
     }
-    ctx.fillStyle="#ffffff";
-    ctx.fillText("FPS: "+framesLastSecond, 10 ,20);
+
+   
+  
     requestAnimationFrame(drawGame);
 }
